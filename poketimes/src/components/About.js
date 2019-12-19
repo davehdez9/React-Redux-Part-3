c930012afbdfd423cbfc746f8a0a941c63cf0095
David <n.dave.hernandez@gmail.com>
2019-12-19 13:46:39 -0500

    using axios

--- poketimes/src/components/About.js
+++ poketimes/src/components/About.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import Rainbow from '../hoc/Rainbow';
 
 const About = () => {
   return (
-    <div className="container">
-      <h4 className="center">About</h4>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo omnis
-        aperiam earum culpa, animi, nulla rem eligendi reiciendis illo inventore
-        aspernatur esse veritatis, quos quam aliquam nisi dolores natus ullam?
-      </p>
+    <div>
+      <div className="container">
+        <h4 className="center">About</h4>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo omnis
+          aperiam earum culpa, animi, nulla rem eligendi reiciendis illo
+          inventore aspernatur esse veritatis, quos quam aliquam nisi dolores
+          natus ullam?
+        </p>
+      </div>
     </div>
   );
 };
 
-export default Rainbow(About);
+export default About;
--- poketimes/src/hoc/Rainbow.js
+++ poketimes/src/hoc/Rainbow.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Rainbow = WrappedComponent => {
   const colours = ['red', 'pink', 'orange', 'blue', 'green', 'yellow'];
-  const randomColour = colours[Math.floor(Math.random() * 5)];
+  const randomColour = colours[Math.floor(Math.random() * 6)];
   const className = randomColour + '-text';
 
   return props => {
